25af533e2e8f213e0efac98c486951fa1f35045c
Micah Pawlicki <mich.pawlicki@gmail.com>
2025-06-12 16:52:17 -0500

    Calculator files.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -9,8 +10,14 @@ client = genai.Client(api_key=api_key)
 if len(sys.argv) == 1:
     print("Error, no prompt provided.")
     sys.exit(1)
-content = sys.argv[1]
-response = client.models.generate_content(model='gemini-2.0-flash-001', contents=content)
+prompt = sys.argv[1]
+messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+]
+response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
