18cc6487771e643c018e1e3d56121bd3f7876c11
Micah Pawlicki <mich.pawlicki@gmail.com>
2025-06-19 15:28:50 -0500

    implemented a function to get a file's content up to a max of 10k

---
+++ functions/get_file_content.py
@@ -0,0 +1,21 @@
+import os
+
+def get_file_content(working_directory, file_path):
+
+    root_path = os.path.abspath(working_directory)
+    joined_path = os.path.abspath(os.path.join(root_path, file_path))
+    MAX_CHARS = 10000
+
+    if not joined_path.startswith(root_path):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(joined_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    with open(joined_path, "r") as f:
+        file_content_string = f.read(MAX_CHARS)
+        
+    if os.path.getsize(joined_path) > len(file_content_string):
+        file_content_string += f' [...File "{file_path}" truncated at 10000 characters]'
+
+    return file_content_string
