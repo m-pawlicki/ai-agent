895cbac78dcc911d65e1bf2e04ed7027bc2ce188
Micah Pawlicki <mich.pawlicki@gmail.com>
2025-06-20 11:02:03 -0500

    Implementing function schemas.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os, sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from function_schemas import *
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,12 +11,46 @@ client = genai.Client(api_key=api_key)
 if len(sys.argv) == 1:
     print("Error, no prompt provided.")
     sys.exit(1)
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+model_name = 'gemini-2.0-flash-001'
 prompt = sys.argv[1]
 messages = [
     types.Content(role="user", parts=[types.Part(text=prompt)]),
 ]
-response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
-print(response.text)
+
+available_functions = types.Tool(
+    function_declarations=[
+         schema_get_files_info,
+         schema_get_file_content,
+         schema_run_python_file,
+         schema_write_file
+    ]
+)
+
+response = client.models.generate_content(
+      model=model_name, 
+      contents=messages,
+      config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt))
+
+if len(response.function_calls) > 0:
+      for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+else:
+     print(response.text)
 
 if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
         print(f"User prompt: {prompt}")
